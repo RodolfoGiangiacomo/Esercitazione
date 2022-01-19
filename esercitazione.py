ae101aa8640e2a0a83bad4223464d393c642d05e
TheRealRodolfo <fffrancescodeplacido@gmail.com>
2022-01-19 10:28:05 +0100

    messaggio

--- esercitazione.py
+++ esercitazione.py
@@ -25,12 +25,43 @@
 # Calling methods from instances of a class: 
 #print(item2.calculate_total_price(item2.price, item2.quantity))
 
-class Item:
-    name = ""
-    price = ""
-    quantità = ""
+#class Item:
+#    name = ""
+#    price = ""
+#    quantity = ""
 
-    def __init__(self, name, prezzo, quantità):
+#    def __init__(self, name, price, quantity):
+#        self.name = name
+#        self.price = price
+#        self.quantity = quantity
+
+#        if quantity < 0:
+#            print("Errore: La quantità non può essere minore di zero.")
+#        if price < 0:
+#            print("Errore: Il prezzo non può essere minore di zero.")
+#        else:
+#            print(name, price, quantity)
+
+#    def calcolo_prezzo_totale(self, price, quantity):
+#        prezzo_totale = price * quantity
+#        print("Il prezzo totale è: " + prezzo_totale)
+
+#itemuno = Item
+#itemuno.__init__()
+#itemuno.calcolo_prezzo_totale()
+
+class Item:
+    def __init__(self, name: str, price: float, quantity= 0):
+        assert price > 0, "Errore: Il prezzo non può essere inferiore di zero"
+        assert quantity > 0, "Errore: la quantità non può essere minore di zero"
+
         self.name = name
-        self.prezzo = prezzo
-        self.quantità = quantità
+        self.price = price
+        self.quantity = quantity
+    def calcola_prezzo_totale(self):
+        return self.price * self.quantity
+
+itemuno = Item("Telefono", 100, 5)
+itemdue = Item("Computer", 200 , 3)
+print(itemuno.calcola_prezzo_totale())
+print(itemdue.calcola_prezzo_totale())
